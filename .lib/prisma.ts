36c5709faaa1d9105152d5fe6a30dda2c3938aa7
THICO <thiago@imaxgames.com.br>
2025-09-09 20:09:23 -0300

    change prisma file

--- .lib/prisma.ts
+++ .lib/prisma.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"], // opcional: log das queries
+    log: ["warn", "error"], // "query" só se precisar debug
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
